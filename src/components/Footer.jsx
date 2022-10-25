c08a428da13f3e8803d67af60e9f4d8a65faa394
fredrikac <fredrika.carlsen@gmail.com>
2022-10-25 16:42:24 +0200

    fix responsive header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
-import styles from './Footer.module.css';
+import s from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={s.footer}>
+      <div className={s.footerLinks}>
       <p>2000-2022</p>
       <a href="">Facebook</a>
       <a href="">Instagram</a>
+      <a href="https://www.flaticon.com/free-icons/open-menu" title="open menu icons">Open menu icon created by Pixel perfect - Flaticon</a> 
+      </div>
     </footer>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,10 @@
-import styles from './Header.module.css';
+import s from './Header.module.css';
 import menuIcon from '../icons/menu.png';
+import cx from 'classnames';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-
-  //denna funkar SÅDÄR. När skärmen är <600px, ska ikonen synas och menyn döljas. 
-  //när skärmen är >600px, ska ikonen döljas och menyn lägga sig längs skärmens fulla längd. 
-  const toggleNavbar = ()=> {
-    console.log('clicking the hamburger')
+  const toggleNavbar = () => {
     const x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
@@ -17,38 +15,22 @@ const Header = () => {
 
   return (
     <header>
-    <div className={styles.topnav}>
-    <h1 className={styles.h1Heading}><a href="/" className={styles.active}>dagensbok.com</a></h1>
-    <nav id="myLinks" className={styles.myLinks}>
-        <a className={styles.headerLinks} href="">Hem</a>
-        <a className={styles.headerLinks} href="">Krönikor</a>
-        <a className={styles.headerLinks} href="">Intervjuer</a>
-        <a className={styles.headerLinks} href="">FAQ</a>
-        <a className={styles.headerLinks} href="">Redaktionen</a>
-        <a className={styles.headerLinks} href="">Arkivet</a>
-    </nav>
-      <img src={menuIcon} className={styles.menuIcon} onClick={()=>toggleNavbar()}>
-      </img>
-  </div>
-
-
-
-
-
-
-  
-      {/* <nav className={styles.headerNav}>
-        <a className={styles.headerLinks} href="">Hem</a>
-        <a className={styles.headerLinks} href="">Krönikor</a>
-        <a className={styles.headerLinks} href="">Intervjuer</a>
-        <a className={styles.headerLinks} href="">FAQ</a>
-        <a className={styles.headerLinks} href="">Redaktionen</a>
-        <a className={styles.headerLinks} href="">Arkivet</a>
-      </nav> */}
+    <div className={cx(s.topnav)}>
+      <h1 className={s.h1Heading}><Link to="/" className={s.h1HeaderLink}>dagensbok.com</Link></h1>
+      <nav id="myLinks" className={s.myLinks}>
+        <a className={s.headerLinks} href="">Hem</a>
+        <a className={s.headerLinks} href="">Krönikor</a>
+        <a className={s.headerLinks} href="">Intervjuer</a>
+        <a className={s.headerLinks} href="">FAQ</a>
+        <a className={s.headerLinks} href="">Redaktionen</a>
+        <a className={s.headerLinks} href="">Arkivet</a>
+      </nav>
+        <img src={menuIcon} className={s.menuIcon} onClick={toggleNavbar} alt='menu-hamburger'>
+        </img>
+      </div>
     </header>
   )
 }
 
 export default Header;
 
-/* <a href="https://www.flaticon.com/free-icons/open-menu" title="open menu icons">Open menu icons created by Pixel perfect - Flaticon</a> */
